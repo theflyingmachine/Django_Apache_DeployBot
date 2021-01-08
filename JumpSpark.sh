634a4d0e4357a7bd687f626cf2491bde703ca84d
Kalloor, Eric Abraham <eric.kalloor@cerner.com>
2021-01-08 17:58:33 +0530

    https support added + code formating

--- JumpSpark.sh
+++ JumpSpark.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
-  
-if [ "$EUID" -ne 0 ]
-  then echo "Please run as root"
-  exit
-fi
 
+if [ "$EUID" -ne 0 ]; then
+        echo "Please run as root"
+        exit
+fi
+https_config='N'
 read -p 'Git Repo: ' gitrepo
 read -p 'Project Name: ' projectname
 read -p 'Site Name: ' sitename
+read -p 'Enable https - Y or N ? (default N): ' https_config
 read -p 'Requirements.txt File Name: ' requirements
 read -p 'Installation App Directory: ' installdir
 
-if [ "${installdir: -1}" == '/' ];then
-installdir="${installdir::-1}"
+if [ "${installdir: -1}" == '/' ]; then
+        installdir="${installdir::-1}"
 fi
-
 
 YUM_PACKAGE_NAME="python3-pip apache2 libapache2-mod-wsgi-py3 git"
 DEB_PACKAGE_NAME="python3-pip apache2 libapache2-mod-wsgi-py3 git"
@@ -22,13 +22,13 @@ DEB_PACKAGE_NAME="python3-pip apache2 libapache2-mod-wsgi-py3 git"
 YUM_CMD=$(which yum)
 APT_GET_CMD=$(which apt-get)
 if [[ ! -z $YUM_CMD ]]; then
-    yum install $YUM_PACKAGE_NAME
- elif [[ ! -z $APT_GET_CMD ]]; then
-    apt-get install $DEB_PACKAGE_NAME
- else
-    echo "error can't install package $PACKAGE"
-    exit 1;
- fi
+        yum install $YUM_PACKAGE_NAME
+elif [[ ! -z $APT_GET_CMD ]]; then
+        apt-get install $DEB_PACKAGE_NAME
+else
+        echo "error can't install package $PACKAGE"
+        exit 1
+fi
 
 #diable firewall
 sudo ufw disable
@@ -43,7 +43,6 @@ git clone $gitrepo
 basename=$(basename $gitrepo)
 repodir=${basename%.*}
 
-
 cd $repodir
 virtualenv venv
 source venv/bin/activate
@@ -51,9 +50,65 @@ pip install -r $requirements
 python manage.py collectstatic
 deactivate
 
-
 #create site.conf
-echo '<VirtualHost *:80>
+if [ "$https_config" = "Y" ] || [ "$https_config" = "y" ]; then
+        echo 'WSGIApplicationGroup %{GLOBAL}
+WSGIDaemonProcess '$projectname' python-home='$installdir'/'$repodir'/venv python-path='$installdir'/'$repodir'
+WSGIProcessGroup '$projectname'
+
+
+<VirtualHost *:80>
+ServerAdmin ericabraham.ea@gmail.com
+ServerName '$sitename'
+DocumentRoot '$installdir'
+
+Alias /static '$installdir'/'$repodir'/staticfiles
+<Directory "'$installdir'/'$repodir'/staticfiles">
+        Options FollowSymLinks
+        Order allow,deny
+        Allow from all
+        Require all granted
+</Directory>
+
+
+ErrorLog ${APACHE_LOG_DIR}/'$projectname'_error.log
+CustomLog ${APACHE_LOG_DIR}/'$projectname'_access.log combined
+
+WSGIScriptAlias / '$installdir'/'$repodir'/'$projectname'/wsgi.py
+
+<Directory '$installdir'/'$repodir'/'$projectname'/>
+        <Files wsgi.py>
+                Require all granted
+        </Files>
+</Directory>
+</VirtualHost>
+<VirtualHost *:443>
+ServerAdmin ericabraham.ea@gmail.com
+ServerName '$sitename'
+DocumentRoot '$installdir'
+
+Alias /static '$installdir'/'$repodir'/staticfiles
+<Directory "'$installdir'/'$repodir'/staticfiles">
+        Options FollowSymLinks
+        Order allow,deny
+        Allow from all
+        Require all granted
+</Directory>
+
+
+ErrorLog ${APACHE_LOG_DIR}/'$projectname'_error.log
+CustomLog ${APACHE_LOG_DIR}/'$projectname'_access.log combined
+
+WSGIScriptAlias / '$installdir'/'$repodir'/'$projectname'/wsgi.py
+
+<Directory '$installdir'/'$repodir'/'$projectname'/>
+        <Files wsgi.py>
+                Require all granted
+        </Files>
+</Directory>
+</VirtualHost>' >/etc/apache2/sites-available/$sitename.conf
+else
+        echo '<VirtualHost *:80>
         ServerAdmin ericabraham.ea@gmail.com
         ServerName '$sitename'
         DocumentRoot '$installdir'
@@ -79,7 +134,8 @@ echo '<VirtualHost *:80>
                         Require all granted
                 </Files>
         </Directory>
-</VirtualHost>' > /etc/apache2/sites-available/$sitename.conf
+</VirtualHost>' >/etc/apache2/sites-available/$sitename.conf
+fi
 
 chown -R www-data:www-data $installdir/$repodir
 cd /etc/apache2/sites-available/
@@ -88,10 +144,5 @@ systemctl restart apache2
 
 echo "====================================================="
 echo "   Your Django Application is sucessfully Deployed   "
-echo "                 Made with <3 by Eric                "		
+echo "                 Made with <3 by Eric                "
 echo "====================================================="
-
-
-
-
-
